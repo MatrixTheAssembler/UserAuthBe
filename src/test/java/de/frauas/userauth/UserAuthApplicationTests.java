f9e191465f070c4ea7e3be309ffed46c1d8712c9
MatrixTheAssembler <52322925+MatrixTheAssembler@users.noreply.github.com>
2022-12-21 19:57:35 +0100

    Initial commit

---
+++ src/test/java/de/frauas/userauth/UserAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package de.frauas.userauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
